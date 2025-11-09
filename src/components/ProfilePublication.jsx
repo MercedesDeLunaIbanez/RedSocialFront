107d1fbca3201710cceb09961114dab46d10c9cd
mercedesVedruna <mercedes.deluna@a.vedrunasevillasj.es>
2025-11-09 18:07:11 +0100

    publicaciones del usuario

---
+++ src/components/ProfilePublication.jsx
@@ -0,0 +1,44 @@
+import { useParams } from "react-router-dom";
+import { usePagination } from "../hooks/usePagination";
+import GetPublication from "./GetPublication";
+
+
+export default function MyPublication() {
+    const { name } = useParams();
+    const { items, page, totalPages, isLoading, isError, error, nextPage, prevPage } =
+    usePagination(`/publications/public/${name}`, 5); // endpoint y tamaño de página
+
+
+  if (isLoading) return <p>Cargando publicaciones...</p>;
+  if (isError) return <p style={{ color: "red" }}>Error: {error.message}</p>;
+
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Publicaciones (página {page + 1} de {totalPages})</h2>
+
+
+      {items.length === 0 && <p>No hay publicaciones disponibles.</p>}
+
+
+      {items.map((pub) => (
+        <GetPublication
+          key={pub.id}
+          authorName={pub.username}
+          text={pub.text}
+          createDate={pub.createDate}
+        />
+      ))}
+
+
+      <div style={{ marginTop: "20px" }}>
+        <button onClick={prevPage} disabled={page === 0}>
+          ← Anterior
+        </button>
+        <button onClick={nextPage} disabled={page >= totalPages - 1} style={{ marginLeft: "10px" }}>
+          Siguiente →
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 // src/pages/ProfilePage.jsx
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
+import ProfilePublication from "../components/ProfilePublication";
 
 
 export default function ProfilePage() {
@@ -11,6 +12,7 @@ export default function ProfilePage() {
       <main style={{ padding: 20 }}>
         <h3>Perfil de {name}</h3>
         <p>Aquí se mostrarán sus publicaciones.</p>
+        <ProfilePublication />
       </main>
     </>
   );
